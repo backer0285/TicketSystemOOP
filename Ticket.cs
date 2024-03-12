ee5421186614ffa229b5914ed4c935d2b5e2ae0d
Brian Ackerman <backer0285@gmail.com>
2024-03-12 18:26:34 -0500

    [1.17] Handle writing embedded commas

--- Ticket.cs
+++ Ticket.cs
@@ -1,11 +1,66 @@
 public class Ticket
 {
     public int ticketID { get; set; }
-    public string summary { get; set; }
-    public string status { get; set; }
-    public string priority { get; set; }
-    public string submitter { get; set; }
-    public string assigned { get; set; }
+    string _summary;
+    public string summary
+    {
+        get
+        {
+            return this._summary;
+        }
+        set
+        {
+            this._summary = CorrectForCommas(value);
+        }
+    }
+    string _status;
+    public string status
+    {
+        get
+        {
+            return this._status;
+        }
+        set
+        {
+            this._status = CorrectForCommas(value);
+        }
+    }
+    string _priority;
+    public string priority
+    {
+        get
+        {
+            return this._priority;
+        }
+        set
+        {
+            this._priority = CorrectForCommas(value);
+        }
+    }    
+    string _submitter;
+    public string submitter
+    {
+        get
+        {
+            return this._submitter;
+        }
+        set
+        {
+            this._submitter = CorrectForCommas(value);
+        }
+    } 
+    string _assigned;
+    public string assigned
+    {
+        get
+        {
+            return this._assigned;
+        }
+        set
+        {
+            this._assigned = CorrectForCommas(value);
+        }
+    }
     public List<string> watching { get; set; }
 
     public Ticket()
@@ -16,5 +71,15 @@ public class Ticket
     public string Display()
     {
         return $"ID: {ticketID}\nSummary: {summary}\nStatus: {status}\nPriority: {priority}\nSubmitter: {submitter}\nAssigned: {assigned}\nWatching: {string.Join(", ", watching)}\n";
+    }
+
+    // method to check for embedded commas and surround with quotes if necessary
+    public string CorrectForCommas(string stringToCheck)
+    {
+        if (stringToCheck.Contains(','))
+        {
+            stringToCheck = "\"" + stringToCheck + "\"";
+        }
+        return stringToCheck;
     }
 }
